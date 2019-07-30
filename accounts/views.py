eb76bcf80462461e96b0e367afbae935a03b5edb
minjoon <albert507@naver.com>
2019-07-31 00:33:45 +0900

    [accounts] sign_in, sign_up, sign_out, home 뷰 작업 without 프론트

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,16 @@
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views import generic
+from django.views.decorators.cache import never_cache
+
 from accounts.forms import UserCreationForm
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.views import LoginView
 
 
-# class SignUp(generic.CreateView):
-#     form_class = UserCreationForm
-#     success_url = reverse_lazy('login')
-#     template_name = 'signup/signup.html'
+# todo: 임시 홈뷰
+class HomeView(generic.TemplateView):
+    template_name = 'home.html'
 
 
 class BeforeSignUpView(generic.TemplateView):
@@ -16,22 +20,20 @@ class BeforeSignUpView(generic.TemplateView):
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     template_name = 'sign_up.html'
-    success_url = reverse_lazy('account:before_sign_up')
-
-    def get(self, request, *args, **kwargs):
-        return super(SignUpView, self).get(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return super(SignUpView, self).post(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super(SignUpView, self).get_context_data(**kwargs)
-        return context
+    success_url = reverse_lazy('accounts:sign_in')
 
 
-class SignInView(generic.FormView):
-    template_name = 'before_sign_up.html'
+class SignInView(LoginView):
+    template_name = 'sign_in.html'
+
+    def get_success_url(self):
+        return reverse_lazy('accounts:home')
 
 
 class SignOutView(generic.RedirectView):
-    pass
+    url = reverse_lazy('accounts:home')
+
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        auth_logout(request)
+        return super(SignOutView, self).dispatch(request, *args, **kwargs)
