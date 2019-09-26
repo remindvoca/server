74e6fc29d0cce131051ce0745351357754c0174a
unknown <albert507@naver.com>
2019-09-26 10:42:47 +0900

    include voca urls

--- remindvoca/urls.py
+++ remindvoca/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls', namespace='account')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('accounts.urls')),
+    path('voca', include('voca.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
