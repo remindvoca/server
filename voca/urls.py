f176e4064d6530badf052e27ba10fe2749c9360a
minjoon <albert507@naver.com>
2019-10-05 17:41:59 +0900

    add voca urls

--- voca/urls.py
+++ voca/urls.py
@@ -6,6 +6,11 @@ app_name='voca'
 urlpatterns = [
     path('main/', mainview.as_view(), name='main'),
     path('upload/', uploadview.as_view(), name='upload'),
-    path('upload/process/', process, name='process' )
+    path('upload/process/', process, name='process' ),
     #path('', views.main, name='main'),
+
+    path('word_books/', WordBookListView.as_view(), name='word_book_list'),
+    path('word_books/<int:word_book_id>/', WordDayLIstView.as_view(), name='word_day_list'),
+    path('word_days/<int:word_day_id>/', WordListView.as_view(), name='word_list'),
+    path('words/<int:word_id>/', WordDetailView.as_view(), name='word_detail'),
 ]
