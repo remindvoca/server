3736b1777b6e0869eba6e890478b369bd5750b0e
minjoon <albert507@naver.com>
2019-10-05 17:41:14 +0900

    add voca views

--- voca/views.py
+++ voca/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_exempt
-from .models import PDFModel
-from .forms import PDF_Form
-from accounts.models import User
+from django.shortcuts import render
 from django.views import generic
 from django.conf import settings
 import os
 import shutil
 
 
-login_url='accounts/sign_in/'
+from accounts.models import User
+from .forms import PDF_Form
+from .models import PDFModel, WordBook, WordDay, Word
+
+login_url = 'accounts/sign_in/'
+
 
 class mainview(generic.TemplateView):
     template_name = 'main.html'
+
 
 #def main(request):
 #    return render(request, 'pages/main.html',{})
@@ -30,6 +32,7 @@ def write_pdf_file(user,pdf):
     with open(os.path.join(settings.MEDIA_ROOT,user, pdf.name), 'wb+') as destination:
             for chunk in pdf.chunks():
                 destination.write(chunk)
+
 
 @login_required(login_url=login_url)
 def process(request):
@@ -70,3 +73,38 @@ def process(request):
     '''
     # return render(request, 'alert.html', {'msg': "Invalid Form for Video object"})
     return render(request, 'alert.html', {'msg': "잘못된 접근입니다"})
+
+
+class WordBookListView(generic.ListView):
+    template_name = 'word_book_list.html'
+    model = WordBook
+
+    def get_queryset(self):
+        queryset = super(WordBookListView, self).get_queryset()
+        return queryset.filter(user=self.request.user)
+
+
+class WordDayLIstView(generic.ListView):
+    template_name = 'word_day_list.html'
+    model = WordDay
+
+    def get_queryset(self):
+        queryset = super(WordDayLIstView, self).get_queryset()
+        word_book = self.kwargs.get('word_book_Id')
+        return queryset.filter(word_book=word_book)
+
+
+class WordListView(generic.ListView):
+    template_name = 'word_list.html'
+    model = Word
+
+    def get_queryset(self):
+        queryset = super(WordListView, self).get_queryset()
+        word_day = self.kwargs.get('word_day_id')
+        return queryset.filter(word_day=word_day)
+
+
+class WordDetailView(generic.DetailView):
+    template_name = 'word_detail.html'
+    pk_url_kwarg = 'word_id'
+    model = Word
