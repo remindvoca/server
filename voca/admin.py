f9ee247785c120ad29821b14c406c66fd70e3890
minjoong507 <albert507@naver.com>
2019-10-06 13:52:35 +0900

    add voca to admin

--- voca/admin.py
+++ voca/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from voca.models import Word, WordDay, WordBook
 
 # Register your models here.
+
+
+@admin.register(WordBook)
+class WordBookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WordDay)
+class WordDayAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    pass
